1a23bd1c89cc184a52517fb4b63f3b3dd3e179db
young <lovetkddud@naver.com>
2025-02-24 14:06:31 +0900

    2025-02-24 React (component)

---
+++ component/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Component, { ClassComponent, FunctionalComponent } from './component/Component'
+import './App.css';
+import CurlyBraces from './component/CurlyBraces';
+
+function App() {
+  return (
+    <div>
+      {/* <ClassComponent />
+      <FunctionalComponent />
+      <Component /> */}
+      <CurlyBraces />
+    </div>
+  );
+}
+
+export default App;
